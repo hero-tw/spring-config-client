f042c1d80a617ae523b6c6c9f101a231190d9642
gambtho <tgamble@thougthworks.com>
2018-10-11 17:14:23 -0400

    more fighting

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,10 @@ ${DOCKER_LOGIN}
 #Store the repositoryUri as a variable
 REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${REPOSITORY_NAME} --region ${REGION} | jq .repositories[].repositoryUri | tr -d '"'`
 
-docker tag com.hero/demo:latest ${REPOSITORY_URI}/config-demo:latest
-docker tag com.hero/demo:latest ${REPOSITORY_URI}/config-demo:${BUILD_NUMBER}
-docker push ${REPOSITORY_URI}/config-demo:latest
-docker push ${REPOSITORY_URI}/config-demo:${BUILD_NUMBER}
+docker tag com.hero/demo:latest ${REPOSITORY_URI}:latest
+docker tag com.hero/demo:latest ${REPOSITORY_URI}:${BUILD_NUMBER}
+docker push ${REPOSITORY_URI}:latest
+docker push ${REPOSITORY_URI}:${BUILD_NUMBER}
 
 #Replace the build number and respository URI placeholders with the constants above
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-v_${BUILD_NUMBER}.json
