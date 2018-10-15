d3de2168f973c728781daada83bf013a5419284c
kebernet <rcooper@thoughtworks.com>
2018-10-15 13:26:59 -0400

    [Robert/Nygel] Trimming the revision number

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ REPOSITORY_NAME=config-demo-dev
 CLUSTER=cluster-dev
 FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' taskdef.json`
 NAME=`sed -n 's/.*"name": "\(.*\)",/\1/p' taskdef.json`
-SERVICE_NAME=${NAME}-service
+SERVICE_NAME=config-demo-service-dev
 
 `$(aws ecr get-login --no-include-email --region us-east-1)`
 
