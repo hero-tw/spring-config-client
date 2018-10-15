5e4be7da07dce3b30046ff8830ca7416f1d2c2ae
kebernet <rcooper@thoughtworks.com>
2018-10-15 13:23:46 -0400

    [Robert/Nygel] Trimming the revision number

--- deploy.sh
+++ deploy.sh
@@ -24,9 +24,7 @@ sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-v_${BUILD_NUMBER}.json --region ${REGION}
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
 #Get latest revision
-REVISION=`aws ecs describe-task-definition --task-definition ${NAME} --region ${REGION} | jq .taskDefinition.revision` | tail -1
-echo "ecs describe-task-definition --task-definition ${NAME} --region ${REGION}"
-aws ecs describe-task-definition --task-definition ${NAME} --region ${REGION}
+REVISION=`aws ecs describe-task-definition --task-definition ${SERVICE_NAME} --region ${REGION} | jq .taskDefinition.revision` | tail -1
 echo "Read revision ${REVISION}"
 
 #Create or update service
