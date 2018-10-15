d6a4eb7f8ee0ca451d27fb2cc89979fed2fb60be
kebernet <rcooper@thoughtworks.com>
2018-10-15 13:22:21 -0400

    [Robert/Nygel] Trimming the revision number

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,7 @@ aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WO
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
 #Get latest revision
 REVISION=`aws ecs describe-task-definition --task-definition ${NAME} --region ${REGION} | jq .taskDefinition.revision` | tail -1
+echo "ecs describe-task-definition --task-definition ${NAME} --region ${REGION}"
 aws ecs describe-task-definition --task-definition ${NAME} --region ${REGION}
 echo "Read revision ${REVISION}"
 
