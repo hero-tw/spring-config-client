9cc18da808c65489c806d8c907d1c9fd00929f3e
gambtho <tgamble@thougthworks.com>
2018-10-15 11:54:46 -0400

    working pull of ssm

--- src/main/java/com/hero/demo/DemoApplication.java
+++ src/main/java/com/hero/demo/DemoApplication.java
@@ -1,23 +1,61 @@
 package com.hero.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.CompositePropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
+
+	@Autowired
+	ConfigurableEnvironment env;
 
 	@Value("${config.name}")
 	String name = "World";
 
 	@RequestMapping("/")
 	public String home() {
+		CompositePropertySource awsParamSources = null;
+		OUTER:
+		for (PropertySource<?> propertySource : env.getPropertySources()) {
+			if (propertySource.getName().equals("bootstrapProperties")) {
+				CompositePropertySource bootstrap = (CompositePropertySource) propertySource;
+				for (PropertySource<?> nestedSource : bootstrap.getPropertySources()) {
+					if (nestedSource.getName().equals("aws-param-store")) {
+						awsParamSources = (CompositePropertySource) nestedSource;
+						break OUTER;
+					}
+				}
+			}
+		}
+		if (awsParamSources == null) {
+			System.out.println("No AWS Parameter Store PropertySource found");
+		} else {
+			System.out.println("Overview of all AWS Param Store property sources:\n");
+			for (PropertySource<?> nestedSource : awsParamSources.getPropertySources()) {
+				EnumerablePropertySource eps = (EnumerablePropertySource) nestedSource;
+				System.out.println(eps.getName() + ":");
+				for (String propName : eps.getPropertyNames()) {
+					System.out.println("\t" + propName + " = " + eps.getProperty(propName));
+				}
+				if (eps.containsProperty("name")) {
+					this.name = Objects.requireNonNull(eps.getProperty("name")).toString();
+				}
+			}
+		}
 		return "Hello " + name;
 	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
