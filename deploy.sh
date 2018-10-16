a84f8252177d44fc9fd4b7ac0a37b5ea14029a03
kebernet <rcooper@thoughtworks.com>
2018-10-16 09:45:41 -0400

    [Robert] Attempting to sort deploy again.

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ docker push ${REPOSITORY_URI}:${BUILD_NUMBER}
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-v_${BUILD_NUMBER}.json
 #Register the task definition in the repository
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-v_${BUILD_NUMBER}.json --region ${REGION}
-SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
+SERVICES=`aws ecs describe-services --services ${FAMILY} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
 #Get latest revision
 echo "---------------------------------------------------------------"
 echo "aws ecs describe-task-definition --task-definition ${FAMILY} --region ${REGION}"
